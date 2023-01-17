1a4413048fec58ae8f9dda5b19837c21801f8aa2
Francis Francisco <franciscapeleto@gmail.com>
2023-01-17 19:42:40 +0000

    add ex005

---
+++ ex005_sucessor_antecessor.py
@@ -0,0 +1,5 @@
+# imprime o número sucessor e o número antecessor do número digitado
+
+n = int(input('Digite um número: '))
+print('O sucessor de {} é {}.'.format(n, n + 1))
+print('O antecessor de {} é {}.'.format(n, n - 1))
