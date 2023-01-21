a9f422add9a78efd1875713164d3814aaace081e
Francis Francisco <franciscapeleto@gmail.com>
2023-01-21 22:05:38 +0000

    add ex009

---
+++ ex009_tabuada.py
@@ -0,0 +1,13 @@
+# imprime a tabuada do número digitado
+
+número = int(input('Digite um número para saber a tabuada dele: '))
+print('{} x 1 = {}'.format(número, número * 1))
+print('{} x 2 = {}'.format(número, número * 2))
+print('{} x 3 = {}'.format(número, número * 3))
+print('{} x 4 = {}'.format(número, número * 4))
+print('{} x 5 = {}'.format(número, número * 5))
+print('{} x 6 = {}'.format(número, número * 6))
+print('{} x 7 = {}'.format(número, número * 7))
+print('{} x 8 = {}'.format(número, número * 8))
+print('{} x 9 = {}'.format(número, número * 9))
+print('{} x 10 = {}'.format(número, número * 10))
