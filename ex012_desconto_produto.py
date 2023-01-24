3c4066b3432707458a656076e24ec8972019769d
Francis Francisco <franciscapeleto@gmail.com>
2023-01-24 18:59:51 +0000

    add ex012

---
+++ ex012_desconto_produto.py
@@ -0,0 +1,4 @@
+# calcula o valor do produto com 5% de desconto
+
+preco = float(input('Qual é o preco do produto? R$'))
+print('O valor do produto com 5% de desconto é de R${:,.2f}'.format(preco * 0.95))
