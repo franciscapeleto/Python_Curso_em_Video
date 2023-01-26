34a1979f6dec5055a3ba18a593e735efe29e690d
Francis Francisco <franciscapeleto@gmail.com>
2023-01-26 13:56:30 +0000

    add ex013

---
+++ ex013_aumento_salário.py
@@ -0,0 +1,4 @@
+# calcula o valor do salário  com 15% de aumento
+
+salário = float(input('Qual é o valor do salário ? '))
+print('O valor do novo salário com 15% de aumento é de R${:,.2f}.'.format(salário * 1.15))
