d2b80d715f7cb9ae8048dadbb531408e44c977c8
Francis Francisco <franciscapeleto@gmail.com>
2023-01-29 21:10:51 +0000

    add ex014

---
+++ ex014_conversor_de_temperaturas.py
@@ -0,0 +1,5 @@
+# converte graus Celsius em graus Fahrenheit
+
+celsius = float(input('Informe a temperatura em Celsius: '))
+fahrenheit = 9 * celsius / 5 + 32
+print('A temperatura de {} graus Celsius corresponde a {} graus Fahrenheit.'.format(celsius, fahrenheit))
