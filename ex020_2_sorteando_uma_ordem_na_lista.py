a7fe34be10e25bdc90072f43eb3390577c0a99d6
Francis Francisco <franciscapeleto@gmail.com>
2023-02-03 15:07:14 +0000

    add ex020

---
+++ ex020_2_sorteando_uma_ordem_na_lista.py
@@ -0,0 +1,9 @@
+# sorteia a ordem que os alunos irão apresentar o trabalho
+from random import randrange
+alunos = ['Roberto', 'Paulo', 'Maria', 'Joaquina']
+n = 1
+while len(alunos) != 0:
+    aluno = alunos[randrange(0, len(alunos))]
+    print('O {}o aluno que irá apresentar o trabalho será "{}".'.format(n, aluno))
+    alunos.remove(aluno)
+    n += 1
