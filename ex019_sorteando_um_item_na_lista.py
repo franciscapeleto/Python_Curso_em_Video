7fb7f6a900768173e47981e89dc6dba3716b8dc6
Francis Francisco <franciscapeleto@gmail.com>
2023-02-03 15:06:15 +0000

    add ex019

---
+++ ex019_sorteando_um_item_na_lista.py
@@ -0,0 +1,5 @@
+# sorteia o nome do aluno que irá apagar a lousa
+from random import choice
+
+alunos = ['Roberto', 'Paulo', 'Maria', 'Joaquina']
+print('O aluno que irá apagar a lousa é "{}".'.format(choice(alunos)))
