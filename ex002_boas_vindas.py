31c5dd904119679ff05f77e89f17c5d677777c07
Francis Francisco <81850446+franciscapeleto@users.noreply.github.com>
2023-02-05 15:08:40 +0000

    Corrigindo a acentuação do ex002

--- ex002_boas_vindas.py
+++ ex002_boas_vindas.py
@@ -1,4 +1,4 @@
-#pergunta nome do usuario e mostra uma mensagem de boas vindas.
+# pergunta nome do usuário e mostra uma mensagem de boas-vindas.
 
 nome = input('Qual é o seu nome? ')
 print('É um grande prazer te conhecer, {}!'.format(nome))
