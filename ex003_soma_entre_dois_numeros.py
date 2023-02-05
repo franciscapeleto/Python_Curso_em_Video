76c16d5dc5e82fc0edf70c15495554e0428c6a53
Francis Francisco <81850446+franciscapeleto@users.noreply.github.com>
2023-02-05 15:10:57 +0000

    Corrigindo a acentuação do ex003

--- ex003_soma_entre_dois_numeros.py
+++ ex003_soma_entre_dois_numeros.py
@@ -1,6 +1,6 @@
-# soma entre dois numeros
+# soma entre dois números
 
-n1 = int(input('Digite um numero: '))  # inserir primeiro numero
-n2 = int(input('Digite outro numero: '))  # inserir segundo numero
-sum = n1 + n2  # variavel com a soma dos dois numeros
-print('A soma entre {} e {} e igual a {}.'.format(n1, n2, sum)) # mostra o resultado da soma dos dois numeros
+n1 = int(input('Digite um número: '))  # inserir primeiro número
+n2 = int(input('Digite outro número: '))  # inserir segundo número
+sum = n1 + n2  # variável com a soma dos dois números
+print('A soma entre {} e {} e igual a {}.'.format(n1, n2, sum)) # mostra o resultado da soma dos dois números
