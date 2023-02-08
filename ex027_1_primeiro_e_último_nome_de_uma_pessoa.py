c86816135e513bb7ea62e512c2653157f207d05d
Francis Francisco <81850446+franciscapeleto@users.noreply.github.com>
2023-02-08 19:29:08 +0000

    add ex027

--- ex027_1_primeiro_e_último_nome_de_uma_pessoa.py
+++ ex027_1_primeiro_e_último_nome_de_uma_pessoa.py
@@ -1,7 +1,7 @@
-# Mostrar o primeiro e ultimo nome do usuário
+# Mostrar o primeiro e último nome do usuário
 
 nomeCompleto = input('Digite seu nome completo: ').strip().title()
 nomeSeparado = nomeCompleto.split()
 print('Muito prazer em te conhecer!')
 print('Seu primeiro nome e "{}"'.format(nomeSeparado[0]))
-print('Seu ultimo nome e "{}"'.format(nomeSeparado[-1]))
+print('Seu último nome e "{}"'.format(nomeSeparado[-1]))
