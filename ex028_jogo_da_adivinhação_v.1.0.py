36546f6d9fd3cf16c75c87ebb3dad4cfed0176dd
Francis Francisco <81850446+franciscapeleto@users.noreply.github.com>
2023-02-08 19:34:21 +0000

    add ex028

--- ex028_jogo_da_adivinhação_v.1.0.py
+++ ex028_jogo_da_adivinhação_v.1.0.py
@@ -1,9 +1,10 @@
-# Jogo de Adivinhacao
+# Jogo de Adivinhação
+
 from random import randint
 from time import sleep
-titulo = '\33[34mVou pensar em um numero entra 0 e 5. Tente adivinhar...'
+título = '\33[34mVou pensar em um número entra 0 e 5. Tente adivinhar...'
 print('\33[33m-=-'*20+'\33[m')
-print('{:^65}'.format(titulo))
+print('{:^65}'.format(título))
 print('\33[33m-=-'*20+'\33[m')
 numero = int(input('Em que numero eu pensei? '))
 sorteio = randint(0,5)
@@ -11,6 +12,6 @@ print('\33[35mPROCESSANDO...\33[m')
 sleep(3)
 
 if numero == sorteio:
-    print('\33[32mPARABENS! Voce conseguiu me vencer!')
+    print('\33[32mPARABÉNS! Você conseguiu me vencer!')
 else:
-    print('\33[31mGANHEI! Eu pensei no numero {} e nao no {}'.format(sorteio, numero))
+    print('\33[31mGANHEI! Eu pensei no número {} e não no {}'.format(sorteio, numero))
