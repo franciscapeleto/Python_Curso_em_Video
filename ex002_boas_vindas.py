17dc14ccd6ca94c65c46af9bdeaaa9e718e136f3
Francis Francisco <81850446+franciscapeleto@users.noreply.github.com>
2023-02-21 18:08:21 +0000

    está funcionando

--- ex002_boas_vindas.py
+++ ex002_boas_vindas.py
@@ -1,4 +1,4 @@
 # pergunta nome do usuário e mostra uma mensagem de boas-vindas.
 
-nome = input('Qual é o seu nome? ')
-print('Prazer te conhecer, {}!'.format(nome))
+nome = input('Qual é o seu nome? ')  # o usuário digita o seu nome
+print('Prazer te conhecer, {}!'.format(nome))  # imprime uma mensagem de boas-vindas com o nome digitado pelo usuário
