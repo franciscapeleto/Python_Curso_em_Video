04de98943162d8fb7b6d72ea289765af6b8a0755
Francis Francisco <81850446+franciscapeleto@users.noreply.github.com>
2023-02-21 18:11:22 +0000

    está funcionando

--- ex003_soma_entre_dois_numeros.py
+++ ex003_soma_entre_dois_numeros.py
@@ -3,4 +3,4 @@
 n1 = int(input('Digite um número: '))  # usuário insere o primeiro número
 n2 = int(input('Digite outro número: '))  # usuário insere o segundo número
 sum = n1 + n2  # variável com a soma dos dois números
-print('A soma entre {} e {} é igual a {}.'.format(n1, n2, sum)) # mostra o resultado da soma dos dois números
+print('A soma entre {} e {} é igual a {}.'.format(n1, n2, sum))  # mostra o resultado da soma dos dois números
